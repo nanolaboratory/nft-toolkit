ee52183ce0b7a0858592d75d2d2e99221690a38f
Nomaan Dossaji <nadossaji@gmail.com>
2021-10-03 22:11:51 -0400

    Added basic testing

--- tests/nft/test_image.py
+++ tests/nft/test_image.py
@@ -1,29 +1,67 @@
+import shutil
 from nft.image import RandomImageGenerator
 from pathlib import Path
+from PIL import Image
 
 import os
+from shutil import rmtree
 import pytest
 
 
-def remove_path(path: Path):
-    if path.is_file() or path.is_symlink():
-        path.unlink()
-        return
-    for p in path.iterdir():
-        remove_path(p)
-    path.rmdir()
-
+def get_image_data(image_path):
+    with Image.open(image_path) as im:
+        return list(im.getdata())
 
 @pytest.fixture()
 def random_image_generator():
-    os.mkdir("/tmp/parts/")
-    yield RandomImageGenerator
-    os.rmdir("/tmp/parts/")
-    remove_path(Path("/tmp/example/"))
+    yield RandomImageGenerator(1, './tests/nft/test_modules/', \
+        './tests/nft/output')
+    
+    if os.path.isdir("./tests/nft/output/"):
+        rmtree('./tests/nft/output')
 
 
-class TestRandomImageGenerator:
-    def test_if_directory_exists(self, random_image_generator):
-        collection = random_image_generator(10, 50, 50, '/tmp/parts/', '/tmp/example/')
-        collection.generate_collection()
-        assert os.path.isdir("/tmp/example/")
+def test_init_and_find_attributes(random_image_generator):
+    assert random_image_generator != None
+
+
+def test_height(random_image_generator):
+    assert random_image_generator.height == 20
+
+
+def test_width(random_image_generator):
+    assert random_image_generator.width == 15
+
+
+def test_file_is_generated(random_image_generator):
+    random_image_generator.generate_collection()
+    assert os.path.isdir("./tests/nft/output/")
+    assert os.path.exists( \
+        "./tests/nft/output/maroon-orange-blue_neon-brown-teal.png")
+
+
+def test_overlay(random_image_generator):
+    random_image_generator.generate_collection()
+    
+    upper_left = [(204, 244, 15, 255)] * 3
+    upper_right = [(68, 60, 20, 255)] * 3
+    lower_left = [(238, 99, 48, 255)] * 3
+    top_lower_right = [(48, 238, 114, 255)] * 3
+    bottom_lower_right = [(0, 88, 255, 255)] * 3
+    bottom_offset = 15 * 17
+
+    pixdata = get_image_data( \
+        "./tests/nft/output/maroon-orange-blue_neon-brown-teal.png")
+
+    for row_idx in range(3):
+        pix_index = row_idx * 15
+
+        assert pixdata[pix_index:pix_index + 3] == upper_left
+        assert pixdata[pix_index + 12:pix_index + 15] == upper_right
+        assert pixdata[pix_index + bottom_offset:pix_index + bottom_offset + 3] == lower_left
+
+        if row_idx != 2:
+            assert pixdata[pix_index + bottom_offset + 12:pix_index + bottom_offset + 15] == top_lower_right
+        else:
+            assert pixdata[pix_index + bottom_offset + 12:pix_index + bottom_offset + 15] == bottom_lower_right
+
