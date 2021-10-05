073513e7c5a328647f3206f1da1380738f33152e
Nomaan Dossaji <nadossaji@gmail.com>
2021-10-05 08:32:24 -0400

    Removed flake8 and fixed black linting

--- nft/image.py
+++ nft/image.py
@@ -31,20 +31,18 @@ class RandomImageGenerator(object):
         generates the images
 
     """
-    def __init__(
-        self,
-        permutations,
-        modules_path,
-        collection_output_path
-    ):
+
+    def __init__(self, permutations, modules_path, collection_output_path):
         self.permutations = permutations
         self.collection_output_path = collection_output_path
 
         self.modules_path = modules_path
         self.modules_list = sorted(os.listdir(self.modules_path))
-        self.modules_list = [module_title for module_title in
-                             self.modules_list if not
-                             module_title.startswith(".")]
+        self.modules_list = [
+            module_title
+            for module_title in self.modules_list
+            if not module_title.startswith(".")
+        ]
 
         if len(self.modules_list) == 0:
             raise AssertionError("No modules")
@@ -55,13 +53,13 @@ class RandomImageGenerator(object):
 
     def find_first_attribute(self):
         for module in self.modules_list:
-            attribute_list = os.listdir("{}/{}".format(
-                self.modules_path, module))
+            attribute_list = os.listdir("{}/{}".format(self.modules_path, module))
 
             for attribute in attribute_list:
                 if not attribute.startswith("."):
                     attribute_path = "{}/{}/{}".format(
-                        self.modules_path, self.modules_list[0], attribute)
+                        self.modules_path, self.modules_list[0], attribute
+                    )
                     return attribute_path
 
         raise Exception("No attributes found.")
@@ -77,8 +75,11 @@ class RandomImageGenerator(object):
 
             module_path = "{}/{}".format(self.modules_path, module)
             module_list = sorted(os.listdir(module_path))
-            module_list = [module_title for module_title in module_list
-                           if not module_title.startswith(".")]
+            module_list = [
+                module_title
+                for module_title in module_list
+                if not module_title.startswith(".")
+            ]
 
             for attribute in module_list:
                 attribute_image_path = "{}/{}".format(module_path, attribute)
@@ -98,26 +99,29 @@ class RandomImageGenerator(object):
             for _, attributes in images_paths.items():
                 attribute_rng = randrange(len(attributes))
                 with Image.open(attributes[attribute_rng]) as attribute_image:
-                    attribute_name += "{}_".format(
-                        self.image_name(attribute_image))
+                    attribute_name += "{}_".format(self.image_name(attribute_image))
 
                     attribute_image_h = attribute_image.height
                     attribute_image_w = attribute_image.width
 
-                    if attribute_image_h != self.height \
-                    and attribute_image_w != self.width:
-                        raise AssertionError("Height and/or width does not match \
+                    if (
+                        attribute_image_h != self.height
+                        and attribute_image_w != self.width
+                    ):
+                        raise AssertionError(
+                            "Height and/or width does not match \
                             {0} != {1} and/or {2} != {3}.".format(
-                                attribute_image_h, self.height,
-                                attribute_image_w, self.width))
+                                attribute_image_h,
+                                self.height,
+                                attribute_image_w,
+                                self.width,
+                            )
+                        )
 
-                    nft_image.paste(
-                        attribute_image,
-                        (0, 0),
-                        mask=attribute_image
-                    )
+                    nft_image.paste(attribute_image, (0, 0), mask=attribute_image)
             nft_image_path = "{}/{}.png".format(
-                self.collection_output_path, attribute_name[:-1])
+                self.collection_output_path, attribute_name[:-1]
+            )
             nft_image.save(nft_image_path)
 
     def generate_collection(self):
--- tests/nft/test_image.py
+++ tests/nft/test_image.py
@@ -14,16 +14,15 @@ def get_image_data(image_path):
 
 @pytest.fixture()
 def random_image_generator():
-    yield RandomImageGenerator(1, "./tests/nft/test_modules/",
-                               "./tests/nft/output")
+    yield RandomImageGenerator(1, "./tests/nft/test_modules/", "./tests/nft/output")
 
     if os.path.isdir("./tests/nft/output/"):
         rmtree("./tests/nft/output")
 
 
 def test_image_dimensions():
-    height, width = RandomImageGenerator.image_dimensions(None,
-        "./tests/nft/test_modules/1-bottom/maroon-orange-blue.png"
+    height, width = RandomImageGenerator.image_dimensions(
+        None, "./tests/nft/test_modules/1-bottom/maroon-orange-blue.png"
     )
     assert height == 20
     assert width == 15
@@ -37,22 +36,17 @@ def test_init_and_find_attributes(random_image_generator):
 def test_file_is_generated(random_image_generator):
     random_image_generator.generate_collection()
     assert os.path.isdir("./tests/nft/output/")
-    assert os.path.exists(
-        "./tests/nft/output/maroon-orange-blue_neon-brown-teal.png")
+    assert os.path.exists("./tests/nft/output/maroon-orange-blue_neon-brown-teal.png")
 
 
 def test_build_image_dict(random_image_generator):
     image_paths = random_image_generator.build_image_dict()
-    assert image_paths == OrderedDict([
-        (
-            '1-bottom', 
-            ['./tests/nft/test_modules//1-bottom/maroon-orange-blue.png']
-        ),
-        (
-            '2-top', 
-            ['./tests/nft/test_modules//2-top/neon-brown-teal.png']
-        )
-    ])
+    assert image_paths == OrderedDict(
+        [
+            ("1-bottom", ["./tests/nft/test_modules//1-bottom/maroon-orange-blue.png"]),
+            ("2-top", ["./tests/nft/test_modules//2-top/neon-brown-teal.png"]),
+        ]
+    )
 
 
 def test_input_image_bottom():
@@ -62,18 +56,21 @@ def test_input_image_bottom():
     lower_right = [(0, 88, 255, 255)] * 3
     bottom_offset = 15 * 17
 
-    pixdata = get_image_data(
-        "./tests/nft/test_modules/1-bottom/maroon-orange-blue.png")
+    pixdata = get_image_data("./tests/nft/test_modules/1-bottom/maroon-orange-blue.png")
 
     for row_idx in range(3):
         pix_index = row_idx * 15
 
-        assert pixdata[pix_index:pix_index + 3] == upper_left
-        assert pixdata[pix_index + 12:pix_index + 15] == upper_right
-        assert pixdata[pix_index + bottom_offset:
-                       pix_index + bottom_offset + 3] == lower_left
-        assert pixdata[pix_index + bottom_offset + 12:
-                       pix_index + bottom_offset + 15] == lower_right
+        assert pixdata[pix_index : pix_index + 3] == upper_left
+        assert pixdata[pix_index + 12 : pix_index + 15] == upper_right
+        assert (
+            pixdata[pix_index + bottom_offset : pix_index + bottom_offset + 3]
+            == lower_left
+        )
+        assert (
+            pixdata[pix_index + bottom_offset + 12 : pix_index + bottom_offset + 15]
+            == lower_right
+        )
 
 
 def test_input_image_top():
@@ -83,19 +80,22 @@ def test_input_image_top():
     lower_right = [(48, 238, 114, 255)] * 3
     bottom_offset = 15 * 17
 
-    pixdata = get_image_data(
-        "./tests/nft/test_modules/2-top/neon-brown-teal.png")
+    pixdata = get_image_data("./tests/nft/test_modules/2-top/neon-brown-teal.png")
 
     for row_idx in range(3):
         pix_index = row_idx * 15
 
-        assert pixdata[pix_index:pix_index + 3] == upper_left
-        assert pixdata[pix_index + 12:pix_index + 15] == upper_right
-        assert pixdata[pix_index + bottom_offset:
-                       pix_index + bottom_offset + 3] == lower_left
+        assert pixdata[pix_index : pix_index + 3] == upper_left
+        assert pixdata[pix_index + 12 : pix_index + 15] == upper_right
+        assert (
+            pixdata[pix_index + bottom_offset : pix_index + bottom_offset + 3]
+            == lower_left
+        )
         if row_idx < 2:
-            assert pixdata[pix_index + bottom_offset + 12:
-                           pix_index + bottom_offset + 15] == lower_right
+            assert (
+                pixdata[pix_index + bottom_offset + 12 : pix_index + bottom_offset + 15]
+                == lower_right
+            )
 
 
 def test_overlay(random_image_generator):
@@ -109,20 +109,26 @@ def test_overlay(random_image_generator):
     bottom_offset = 15 * 17
 
     pixdata = get_image_data(
-        "./tests/nft/output/maroon-orange-blue_neon-brown-teal.png")
+        "./tests/nft/output/maroon-orange-blue_neon-brown-teal.png"
+    )
 
     for row_idx in range(3):
         pix_index = row_idx * 15
 
-        assert pixdata[pix_index:pix_index + 3] == upper_left
-        assert pixdata[pix_index + 12:pix_index + 15] == upper_right
-        assert pixdata[pix_index + bottom_offset:
-                       pix_index + bottom_offset + 3] == lower_left
+        assert pixdata[pix_index : pix_index + 3] == upper_left
+        assert pixdata[pix_index + 12 : pix_index + 15] == upper_right
+        assert (
+            pixdata[pix_index + bottom_offset : pix_index + bottom_offset + 3]
+            == lower_left
+        )
 
         if row_idx != 2:
-            assert pixdata[pix_index + bottom_offset + 12:
-                           pix_index + bottom_offset + 15] == top_lower_right
+            assert (
+                pixdata[pix_index + bottom_offset + 12 : pix_index + bottom_offset + 15]
+                == top_lower_right
+            )
         else:
-            assert pixdata[pix_index + bottom_offset + 12:
-                           pix_index + bottom_offset + 15] \
-                   == bottom_lower_right
+            assert (
+                pixdata[pix_index + bottom_offset + 12 : pix_index + bottom_offset + 15]
+                == bottom_lower_right
+            )
