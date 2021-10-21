73fe60034d08caa2f8b5830761a9bcc4fba35e59
Nomaan Dossaji <ndossaji@users.noreply.github.com>
2021-10-21 08:02:12 -0500

    Updated version in setup.py (#9)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="nft-toolkit",
-    version="0.1.0",
+    version="0.1.1",
     author="Nano Labs LLC",
     author_email="hello@nanolabs.dev",
     description="A tool to generate a randomized 2D-image \
